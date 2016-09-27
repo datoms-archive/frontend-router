15456d00ace02786be3bdeb50e3a628c8c655b43
Nataraj Sahoo <nataraj@phoenixrobotix.com>
2016-09-27 14:55:08 +0530

    Used function instead of class for es6 module export

--- index.js
+++ index.js
@@ -12,57 +12,39 @@
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
+var frontendRouter;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+frontendRouter.url = function () {
+	return decodeURI(window.location.hash).substr(1);
+};
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var frontendRouter = function () {
-	function frontendRouter() {
-		_classCallCheck(this, frontendRouter);
+frontendRouter.page = function () {
+	var page = decodeURI(window.location.hash).substr(1).split('?')[0];
+	if (page.slice(-1) == '/') {
+		page = page.slice(0, -1);
 	}
+	return page;
+};
 
-	_createClass(frontendRouter, [{
-		key: 'url',
-		value: function url() {
-			return decodeURI(window.location.hash).substr(1);
-		}
-	}, {
-		key: 'page',
-		value: function page() {
-			var page = decodeURI(window.location.hash).substr(1).split('?')[0];
-			if (page.slice(-1) == '/') {
-				page = page.slice(0, -1);
-			}
-			return page;
-		}
-	}, {
-		key: 'queryString',
-		value: function queryString() {
-			return decodeURI(window.location.hash).substr(1).split('?')[1];
-		}
-	}, {
-		key: 'queryParams',
-		value: function queryParams() {
-			var query_params_json = {};
-			var query_string = decodeURI(window.location.hash).substr(1).split('?')[1];
-			if (query_string) {
-				query_string.split('&').map(function (query) {
-					query = query.split('=');
-					query_params_json[query[0]] = query[1];
-				});
-			}
-			return query_params_json;
-		}
-	}, {
-		key: 'goToPage',
-		value: function goToPage(page) {
-			window.location.hash = page;
-		}
-	}]);
+frontendRouter.queryString = function () {
+	return decodeURI(window.location.hash).substr(1).split('?')[1];
+};
 
-	return frontendRouter;
-}();
+frontendRouter.queryParams = function () {
+	var query_params_json = {};
+	var query_string = decodeURI(window.location.hash).substr(1).split('?')[1];
+	if (query_string) {
+		query_string.split('&').map(function (query) {
+			query = query.split('=');
+			query_params_json[query[0]] = query[1];
+		});
+	}
+	return query_params_json;
+};
+
+frontendRouter.goToPage = function (page) {
+	window.location.hash = page;
+};
 
 exports.default = frontendRouter;
 return FrontendRouter;
--- src/frontendRouter.js
+++ src/frontendRouter.js
@@ -1,33 +1,35 @@
-export default class frontendRouter {
-	url() {
-		return decodeURI(window.location.hash).substr(1);
-	}
+var frontendRouter;
 
-	page() {
-		var page = decodeURI(window.location.hash).substr(1).split('?')[0];
-		if(page.slice(-1) == '/') {
-			page = page.slice(0, -1);
-		}
-		return page;
-	}
-
-	queryString() {
-		return decodeURI(window.location.hash).substr(1).split('?')[1];
-	}
-
-	queryParams() {
-		var query_params_json = {};
-		var query_string = decodeURI(window.location.hash).substr(1).split('?')[1];
-		if(query_string) {
-			query_string.split('&').map(function (query) {
-				query = query.split('=');
-				query_params_json[query[0]] = query[1];
-			});
-		}
-		return query_params_json;
-	}
-
-	goToPage(page) {
-		window.location.hash = page;
-	}
+frontendRouter.url = function() {
+	return decodeURI(window.location.hash).substr(1);
 }
+
+frontendRouter.page = function() {
+	var page = decodeURI(window.location.hash).substr(1).split('?')[0];
+	if(page.slice(-1) == '/') {
+		page = page.slice(0, -1);
+	}
+	return page;
+}
+
+frontendRouter.queryString = function() {
+	return decodeURI(window.location.hash).substr(1).split('?')[1];
+}
+
+frontendRouter.queryParams = function() {
+	var query_params_json = {};
+	var query_string = decodeURI(window.location.hash).substr(1).split('?')[1];
+	if(query_string) {
+		query_string.split('&').map(function (query) {
+			query = query.split('=');
+			query_params_json[query[0]] = query[1];
+		});
+	}
+	return query_params_json;
+}
+
+frontendRouter.goToPage = function(page) {
+	window.location.hash = page;
+}
+
+export default frontendRouter;
